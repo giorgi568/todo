bd7673573603ca196357d0a8ac0d96dc90c77723
giorgi568 <mateshvili02@mail.ru>
2023-06-15 02:49:55 +0400

    added ui to add task

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import './style.css';
 import { initializePage } from './initialize';
 import 'flowbite';
 import { addProject } from './addproject';
+import { createTask } from './createTasks';
 
 // initializePage();
 
-const addProjectbtn = document.getElementById('addProjectSubmit');
-addProjectbtn.addEventListener('click', (e) => {
+const addProjectBtn = document.getElementById('addProjectSubmit');
+addProjectBtn.addEventListener('click', (e) => {
   e.preventDefault();
   addProject();
+});
+
+const addTaskBtn = document.getElementById('createTaskButton');
+addTaskBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const taskName = document.getElementById('taskName').value;
+  const taskDate = document.getElementById('taskDate').value;
+  let taskPriority = 'low';
+  const taskPriorityHigh = document.getElementById('taskPriorityHigh');
+  if (taskPriorityHigh.checked) {
+    taskPriority = 'high';
+  }
+  console.log(createTask(taskName, taskDate, taskPriority));
 });
