677de521ca282723777b15f9d0e67e1c1a8618b5
giorgi568 <mateshvili02@mail.ru>
2023-06-18 03:02:42 +0400

    addend project remove button

--- src/addproject.js
+++ src/addproject.js
@@ -21,9 +21,33 @@ function addProject(tasks) {
     'border-slate-600',
     'hover:border-transparent',
     'rounded',
-    'text-base'
+    'text-base',
+    'flex',
+    'justify-between'
   );
   newProjectBtn.textContent = PjInput;
+
+  const closeBtn = document.createElement('button');
+  newProjectBtn.appendChild(closeBtn);
+
+  const svgProject = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'svg'
+  );
+  const svgProjectPath = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'path'
+  );
+  svgProject.setAttribute('viewBox', '0 -960 960 960');
+  svgProjectPath.setAttribute(
+    'd',
+    'm249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z'
+  );
+  svgProject.classList.add('fill-current', 'w-5', 'h-5', 'm-0');
+  svgProject.appendChild(svgProjectPath);
+
+  closeBtn.appendChild(svgProject);
+
   listOfProjects.appendChild(newProjectBtn);
   newProjectBtn.addEventListener('click', () => {
     // const rightPanel = document.getElementById('right-panel');
@@ -36,6 +60,10 @@ function addProject(tasks) {
     const h2 = document.getElementById('project-title-header');
     h2.textContent = PjInput;
     displayTask(tasks, PjInput);
+  });
+
+  closeBtn.addEventListener('click', () => {
+    newProjectBtn.remove();
   });
 }
 
